aabed875f6ba8d2e92e035f214d9247c2356a37e
MsFaxe <anty.wjj@gmail.com>
2021-03-01 18:58:31 +0100

    Creating "Start" class responsible for determining the number of rounds

---
+++ src/main/java/com/rps/rockpaperscissors/model/Start.java
@@ -0,0 +1,30 @@
+package com.rps.rockpaperscissors.model;
+
+import lombok.Getter;
+
+import java.util.Scanner;
+
+@Getter
+public class Start {
+    private String name;
+    private int rounds = 0;
+
+    public void enterYourName() {
+        System.out.println("Enter your name, please.");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.print("Hi " + getName());
+    }
+
+    public void enterTheNumberOfRounds() {
+        System.out.println("! Enter number of round, please.");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            rounds = scanner.nextInt();
+            System.out.println(getName() + ", you will play " + rounds + " rounds against the computer. Good luck :)");
+        } catch (Exception e) {
+            System.out.print("Error");
+            enterTheNumberOfRounds();
+        }
+    }
+}
